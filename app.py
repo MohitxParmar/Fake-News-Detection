93a13c2953f1d6a11987c9b1f2b91851391dba79
Mohit <parmarmohit693@gmail.com>
2021-04-02 21:08:26 +0530

    Updated websites and added signup page

--- app.py
+++ app.py
@@ -1,15 +1,42 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response, session, redirect, url_for
 import pickle
 import numpy as np
 import pandas as pd
+import sqlite3
 from sklearn.feature_extraction.text import TfidfVectorizer
+from functools import wraps
+import os
 
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
-@app.route('/')
+
+conn = sqlite3.connect("signupDetails.db")
+c= conn.cursor()
+c.execute("CREATE TABLE IF NOT EXISTS users(username TEXT, email TEXT, password TEXT)")
+conn.commit()
+conn.close()
+
+@app.route('/', methods = ["GET","POST"])
 def man():
-    return render_template('home.html')
+    msg = None
+    if(request.method == "POST"):
+        if(request.form["username"]!="" and request.form["password"]!=""):
+            username = request.form["username"]
+            emailId = request.form["email"]
+            password = request.form["password"]
+            conn = sqlite3.connect("signupDetails.db")
+            c= conn.cursor()
+            c.execute("INSERT INTO users VALUES('"+username+"','"+emailId+"','"+password+"')")
+            conn.commit()
+            conn.close()
+            msg="Your account is created"
+        else:
+            msg = "Please fill all the entries"
+
+    return render_template('sign_up.html',msg = msg)
+
 
 
 @app.route('/predict', methods=['POST'])
@@ -37,6 +64,49 @@ def home():
 
     return render_template('after.html', data = pred)
 
+@app.route("/login", methods = ["GET","POST"])
+def login():
+    r =""
+    msg = ""
+    if(request.method == "POST"):
+        if(request.form["username"]!="" and request.form["password"]!=""):
+            username = request.form["username"]
+            password = request.form["password"]
+            conn = sqlite3.connect("signupDetails.db")
+            c= conn.cursor()
+            c.execute("SELECT * FROM users WHERE username ='"+username+"' and password ='"+password+"'")
+            r = c.fetchall()
+            for i in r:
+                if(username == i[0] and password == i[2]):
+                    session["logedin"] = True
+                    session["username"] = username
+                    session["user_id"]=r[0][0]
+                    return redirect("index")
+                else:
+                    msg= "Please enter valid username and password"
+
+            
+            
+    return render_template("login.html",msg = msg)
+
+@app.route("/index")
+def index():
+    if 'user_id' in session:
+        return render_template("home.html")
+    else:
+        return redirect('/')
+
+# @app.route("/logout")
+# def logout():
+#     session.clear()
+#     return redirect(url_for("login"))
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id')
+    return redirect('/')
+
 if __name__ == "__main__":
+    # app.secret_key = 'some secret key'
     app.run(debug = True)
 
